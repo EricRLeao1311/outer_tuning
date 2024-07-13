1990c9b6161203dc3fc732e59f9493d2050186f9
Eric Leao <ericruasleao@aluno.puc-rio.br>
2024-07-13 20:24:54 -0300

    Adicionando de Forma Manual sugestões geradas pela OnDBTuning

--- src/main/java/br/com/pucrio/inf/biobd/outertuning/agents/OuterTuningAgent.java
+++ src/main/java/br/com/pucrio/inf/biobd/outertuning/agents/OuterTuningAgent.java
@@ -80,8 +80,10 @@ public class OuterTuningAgent implements Runnable {
         while (this.running) {
             try {
                 captor.verifyDatabase();
-                this.addRealisticTestActions(); // Adicionando várias ações de teste realista
-                // this.executeDispatcher();
+                if (isLucianaHeuristicSelected) {
+                    this.addRealisticTestActions(); // Adicionando várias ações de teste realista
+                }
+                this.executeDispatcher();
                 sleep(Integer.parseInt(config.getProperty("intervalToCapture")));
             } catch (InterruptedException e) {
                 log.error(e);
@@ -137,169 +139,91 @@ public class OuterTuningAgent implements Runnable {
         try {
             List<ActionSF> testActions = new ArrayList<>();
     
-            // Ação 1: Índice para CUSTOMER(C_NATIONKEY)
-            ActionSF action1 = new ActionSF();
-            action1.setId("1");
-            action1.setName("Index on CUSTOMER(C_NATIONKEY)");
-            action1.setHeuristic("Index");
-            action1.setCommand("CREATE INDEX idx_customer_nationkey ON CUSTOMER(C_NATIONKEY);");
-            action1.setBonus(8.0f);
-            action1.setCreationCost(4.0f);
-            action1.setCost(1.0f);
-            action1.setType("Index");
-            action1.setStatus("created");
-            action1.setJustify("Index to speed up nation key lookups in CUSTOMER.");
-            testActions.add(action1);
-    {
-            // Ação 2: Índice para ORDERS(O_ORDERDATE)
-            ActionSF action2 = new ActionSF();
-            action2.setId("2");
-            action2.setName("Index on ORDERS(O_ORDERDATE)");
-            action2.setHeuristic("Index");
-            action2.setCommand("CREATE INDEX idx_orders_orderdate ON ORDERS(O_ORDERDATE);");
-            action2.setBonus(7.5f);
-            action2.setCreationCost(3.5f);
-            action2.setCost(1.2f);
-            action2.setType("Index");
-            action2.setStatus("created");
-            action2.setJustify("Index to speed up order date lookups in ORDERS.");
-            testActions.add(action2);
+            // Ação específica com base na imagem fornecida
+            ActionSF newAction = new ActionSF();
+            newAction.setId("HYP_O_CUSTKEY_O_ORDERDATE");
+            newAction.setName("Hypothetical Index on Customer Key and Order Date");
+            newAction = this.getActionFromList(newAction);
+            newAction.setCommand("CREATE INDEX HYP_O_CUSTKEY_O_ORDERDATE ON orders(o_custkey, o_orderdate)");
+            newAction.setJustify("Not yet!");
+            newAction.setHeuristic("HeuristicaIndicesDinamicos");
+            newAction.addSql(captor.getSqlCaptured("/* TPC_H Query 5 */ SELECT n_name, sum(l_extendedprice * (1 - l_discount)) as revenue FROM customer, orders, lineitem, supplier, nation, region WHERE c_custkey = o_custkey AND l_orderkey = o_orderkey AND l_suppkey = s_suppkey AND c_nationkey = s_nationkey AND s_nationkey = n_nationkey AND n_regionkey = r_regionkey AND r_name = 'MIDDLE EAST' AND o_orderdate >= date '1997-12-07' AND o_orderdate < date '1997-12-07' + interval '1' year GROUP BY n_name ORDER BY revenue desc"));
+            newAction.setStatus("suggested");
+            newAction.setBonus(0.75f);
+            newAction.setCreationCost(13.2f); 
+            newAction.setCost(0.5f);
+            newAction.setType("Index");
+
+            // Ação específica com base nas inferências fornecidas
+            ActionSF newAction_2 = new ActionSF();
+            newAction_2.setId("HYP_O_ORDERDATE_O_CUSTKEY");
+            newAction_2.setName("Hypothetical Index on Order Date and Customer Key");
+            newAction_2 = this.getActionFromList(newAction_2);
+            newAction_2.setCommand("CREATE INDEX HYP_O_ORDERDATE_O_CUSTKEY ON orders(o_orderdate, o_custkey)");
+            newAction_2.setJustify("Not yet!");
+            newAction_2.setHeuristic("HeuristicaIndicesDinamicos");
+            newAction_2.addSql(captor.getSqlCaptured("/* TPC_H Query 5 */ SELECT n_name, sum(l_extendedprice * (1 - l_discount)) as revenue FROM customer, orders, lineitem, supplier, nation, region WHERE c_custkey = o_custkey AND l_orderkey = o_orderkey AND l_suppkey = s_suppkey AND c_nationkey = s_nationkey AND s_nationkey = n_nationkey AND n_regionkey = r_regionkey AND r_name = 'MIDDLE EAST' AND o_orderdate >= date '1997-12-07' AND o_orderdate < date '1997-12-07' + interval '1' year GROUP BY n_name ORDER BY revenue desc"));
+            newAction_2.setStatus("suggested");
+            newAction_2.setBonus(15.6f);
+            newAction_2.setCreationCost(7.5f); 
+            newAction_2.setCost(0.5f);
+            newAction_2.setType("Index");
+
+            // Ação específica com base nas inferências fornecidas
+            ActionSF newAction3 = new ActionSF();
+            newAction3.setId("HYP_L_ORDERKEY_L_PARTKEY");
+            newAction3.setName("Hypothetical Index on Lineitem Order Key and Part Key");
+            newAction3 = this.getActionFromList(newAction3);
+            newAction3.setCommand("CREATE INDEX HYP_L_ORDERKEY_L_PARTKEY ON lineitem(l_orderkey, l_partkey)");
+            newAction3.setJustify("Not yet!");
+            newAction3.setHeuristic("HeuristicaIndiceHipoteticoBeneficios");
+            newAction3.addSql(captor.getSqlCaptured("/* TPC_H Query 5 */ SELECT n_name, sum(l_extendedprice * (1 - l_discount)) as revenue FROM customer, orders, lineitem, supplier, nation, region WHERE c_custkey = o_custkey AND l_orderkey = o_orderkey AND l_suppkey = s_suppkey AND c_nationkey = s_nationkey AND s_nationkey = n_nationkey AND n_regionkey = r_regionkey AND r_name = 'MIDDLE EAST' AND o_orderdate >= date '1997-12-07' AND o_orderdate < date '1997-12-07' + interval '1' year GROUP BY n_name ORDER BY revenue desc"));
+            newAction3.setStatus("suggested");
+            newAction3.setBonus(0.2f);
+            newAction3.setCreationCost(17.8f); 
+            newAction3.setCost(0.5f);
+            newAction3.setType("Index");
+
+            // Ação específica com base nas inferências fornecidas
+            ActionSF newAction4 = new ActionSF();
+            newAction4.setId("HYP_L_COMMITDATE_L_ORDERKEY");
+            newAction4.setName("Hypothetical Index on Lineitem Commit Date and Order Key");
+            newAction4 = this.getActionFromList(newAction4);
+            newAction4.setCommand("CREATE INDEX HYP_L_COMMITDATE_L_ORDERKEY ON lineitem(l_commitdate, l_orderkey)");
+            newAction4.setJustify("Not yet!");
+            newAction4.setHeuristic("HeuristicaIndicesDinamicos");
+            newAction4.addSql(captor.getSqlCaptured("/* TPC_H Query 4 */ select o_orderpriority, count(*) as order_count from orders where o_orderdate >= date '1994-06-22' and o_orderdate < adddate('1994-06-22', interval '3' month) and exists ( select * from lineitem where l_orderkey = o_orderkey and l_commitdate < l_receiptdate ) group by o_orderpriority order by o_orderpriority"));
+            newAction4.setStatus("suggested");
+            newAction4.setBonus(0.9f);
+            newAction4.setCreationCost(22.4f); 
+            newAction4.setCost(0.5f);
+            newAction4.setType("Index");
+            // Ação específica com base nas inferências fornecidas
+            ActionSF newAction6 = new ActionSF();
+            newAction6.setId("HYP_O_ORDERKEY_O_ORDERDATE");
+            newAction6.setName("Hypothetical Index on Order Key and Order Date");
+            newAction6 = this.getActionFromList(newAction6);
+            newAction6.setCommand("CREATE INDEX HYP_O_ORDERKEY_O_ORDERDATE ON orders(o_orderkey, o_orderdate)");
+            newAction6.setJustify("Not yet!");
+            newAction6.setHeuristic("HeuristicaIndicesDinamicos");
+            newAction6.addSql(captor.getSqlCaptured("/* TPC_H Query 3 */ SELECT l_orderkey, sum(l_extendedprice * (1 - l_discount)) as revenue, o_orderdate, o_shippriority FROM customer, orders, lineitem WHERE c_mktsegment = 'HOUSEHOLD' AND c_custkey = o_custkey AND l_orderkey = o_orderkey AND o_orderdate < date '1996-09-21' AND l_shipdate > date '1993-05-16' GROUP BY l_orderkey, o_orderdate, o_shippriority ORDER BY revenue desc, o_orderdate LIMIT 20"));
+            newAction6.setStatus("suggested");
+            newAction6.setBonus(1.0f);
+            newAction6.setCreationCost(8.0f);
+            newAction6.setCost(0.5f);
+            newAction6.setType("Index");
+
+
+
+
     
-            // Ação 3: Índice para LINEITEM(L_RETURNFLAG)
-            ActionSF action3 = new ActionSF();
-            action3.setId("3");
-            action3.setName("Index on LINEITEM(L_RETURNFLAG)");
-            action3.setHeuristic("Index");
-            action3.setCommand("CREATE INDEX idx_lineitem_returnflag ON LINEITEM(L_RETURNFLAG);");
-            action3.setBonus(6.0f);
-            action3.setCreationCost(3.0f);
-            action3.setCost(1.1f);
-            action3.setType("Index");
-            action3.setStatus("created");
-            action3.setJustify("Index to speed up return flag lookups in LINEITEM.");
-            testActions.add(action3);
-    
-            // Ação 4: Índice para PART(P_SIZE, P_TYPE)
-            ActionSF action4 = new ActionSF();
-            action4.setId("4");
-            action4.setName("Index on PART(P_SIZE, P_TYPE)");
-            action4.setHeuristic("Index");
-            action4.setCommand("CREATE INDEX idx_part_size_type ON PART(P_SIZE, P_TYPE);");
-            action4.setBonus(9.0f);
-            action4.setCreationCost(4.5f);
-            action4.setCost(1.5f);
-            action4.setType("Index");
-            action4.setStatus("created");
-            action4.setJustify("Index to speed up size and type lookups in PART.");
-            testActions.add(action4);
-    
-            // Ação 5: Visão Materializada para CUSTOMER, ORDERS, e LINEITEM
-            ActionSF action5 = new ActionSF();
-            action5.setId("5");
-            action5.setName("Materialized View for CUSTOMER, ORDERS, and LINEITEM");
-            action5.setHeuristic("Materialized View");
-            action5.setCommand("CREATE MATERIALIZED VIEW mv_customer_orders_lineitem AS " +
-                    "SELECT C.C_CUSTKEY, C.C_NAME, O.O_ORDERKEY, L.L_EXTENDEDPRICE, L.L_DISCOUNT, O.O_ORDERDATE " +
-                    "FROM CUSTOMER C " +
-                    "JOIN ORDERS O ON C.C_CUSTKEY = O.O_CUSTKEY " +
-                    "JOIN LINEITEM L ON O.O_ORDERKEY = L.L_ORDERKEY;");
-            action5.setBonus(20.0f);
-            action5.setCreationCost(15.0f);
-            action5.setCost(7.0f);
-            action5.setType("Materialized View");
-            action5.setStatus("created");
-            action5.setJustify("Materialized view to optimize join operations among CUSTOMER, ORDERS, and LINEITEM.");
-            testActions.add(action5);
-    
-            // Ação 6: Índice para SUPPLIER(S_NATIONKEY)
-            ActionSF action6 = new ActionSF();
-            action6.setId("6");
-            action6.setName("Index on SUPPLIER(S_NATIONKEY)");
-            action6.setHeuristic("Index");
-            action6.setCommand("CREATE INDEX idx_supplier_nationkey ON SUPPLIER(S_NATIONKEY);");
-            action6.setBonus(5.5f);
-            action6.setCreationCost(2.5f);
-            action6.setCost(1.0f);
-            action6.setType("Index");
-            action6.setStatus("created");
-            action6.setJustify("Index to speed up nation key lookups in SUPPLIER.");
-            testActions.add(action6);
-    
-            // Ação 7: Índice para PARTSUPP(PS_PARTKEY, PS_SUPPKEY)
-            ActionSF action7 = new ActionSF();
-            action7.setId("7");
-            action7.setName("Index on PARTSUPP(PS_PARTKEY, PS_SUPPKEY)");
-            action7.setHeuristic("Index");
-            action7.setCommand("CREATE INDEX idx_partsupp_partkey_suppkey ON PARTSUPP(PS_PARTKEY, PS_SUPPKEY);");
-            action7.setBonus(10.0f);
-            action7.setCreationCost(5.0f);
-            action7.setCost(2.0f);
-            action7.setType("Index");
-            action7.setStatus("created");
-            action7.setJustify("Index to speed up join operations between PARTSUPP and other tables.");
-            testActions.add(action7);
-    
-            // Ação 8: Visão Materializada para LINEITEM e ORDERS
-            ActionSF action8 = new ActionSF();
-            action8.setId("8");
-            action8.setName("Materialized View for LINEITEM and ORDERS");
-            action8.setHeuristic("Materialized View");
-            action8.setCommand("CREATE MATERIALIZED VIEW mv_lineitem_orders AS " +
-                    "SELECT L.L_ORDERKEY, SUM(L.L_EXTENDEDPRICE * (1 - L.L_DISCOUNT)) AS REVENUE, O.O_ORDERDATE " +
-                    "FROM LINEITEM L " +
-                    "JOIN ORDERS O ON L.L_ORDERKEY = O.O_ORDERKEY " +
-                    "GROUP BY L.L_ORDERKEY, O.O_ORDERDATE;");
-            action8.setBonus(18.0f);
-            action8.setCreationCost(14.0f);
-            action8.setCost(6.5f);
-            action8.setType("Materialized View");
-            action8.setStatus("created");
-            action8.setJustify("Materialized view to optimize aggregation and join operations between LINEITEM and ORDERS.");
-            testActions.add(action8);
-    
-            // Ação 9: Índice para LINEITEM(L_SHIPDATE)
-            ActionSF action9 = new ActionSF();
-            action9.setId("9");
-            action9.setName("Index on LINEITEM(L_SHIPDATE)");
-            action9.setHeuristic("Index");
-            action9.setCommand("CREATE INDEX idx_lineitem_shipdate ON LINEITEM(L_SHIPDATE);");
-            action9.setBonus(8.5f);
-            action9.setCreationCost(4.0f);
-            action9.setCost(1.3f);
-            action9.setType("Index");
-            action9.setStatus("created");
-            action9.setJustify("Index to speed up ship date lookups in LINEITEM.");
-            testActions.add(action9);
-    }
-            // Ação 10: Visão Materializada para SUPPLIER, LINEITEM, e NATION
-            ActionSF action10 = new ActionSF();
-            action10.setId("10");
-            action10.setName("Materialized View for SUPPLIER, LINEITEM, and NATION");
-            action10.setHeuristic("Materialized View");
-            action10.setCommand("CREATE MATERIALIZED VIEW mv_supplier_lineitem_nation AS " +
-                    "SELECT S.S_NAME, S.S_ADDRESS, L.L_QUANTITY, N.N_NAME " +
-                    "FROM SUPPLIER S " +
-                    "JOIN LINEITEM L ON S.S_SUPPKEY = L.L_SUPPKEY " +
-                    "JOIN NATION N ON S.S_NATIONKEY = N.NATIONKEY;");
-            action10.setBonus(22.0f);
-            action10.setCreationCost(16.0f);
-            action10.setCost(8.0f);
-            action10.setType("Materialized View");
-            action10.setStatus("created");
-            action10.setJustify("Materialized view to optimize join operations between SUPPLIER, LINEITEM, and NATION.");
-            testActions.add(action10);
-    
-            for (ActionSF action : testActions) {
-                log.msg("Adicionando ação de teste realista: " + action.getName());
-                this.actionsSF.add(action);
-            }
-    
-            log.msg("Ações de teste realista adicionadas com sucesso.");
+            log.msg("Ação de teste específica adicionada com sucesso.");
         } catch (Exception e) {
-            log.error("Erro ao criar as ações de teste realista: " + e.getMessage());
+            log.error("Erro ao criar a ação de teste específica: " + e.getMessage());
             e.printStackTrace();
         }
-    }    
+    }
+    
 
     public ArrayList<Heuristic> getAllHeuristics() {
         return this.ontology.getAllHeuristics();
